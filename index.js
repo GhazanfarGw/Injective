b50388fb866a72e7b054eb5520dc65558648a8c3
Dan Van <116085344+GhazanfarGw@users.noreply.github.com>
2024-08-02 01:55:03 +0500

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ dotenv.config();
 // app.use(limiter);
 
 const app = express();
-const port = process.env.HTTP_PORT || 3000;
+const port = process.env.port|| 3000;
 
 
 // Alchemy API setup
@@ -427,7 +427,7 @@ app.get('/data', async (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running on HTTP_PORT ${port}`);
+    console.log(`Server running on port ${port}`);
 });
 
 // const express = require('express');
